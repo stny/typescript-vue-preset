f97d5a9915efa4d5863778007bd87e52eb72ecd1
Naoya SATO <stny@colourful.jp>
2018-08-22 10:14:39 +0900

    Use uglifyjs

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
